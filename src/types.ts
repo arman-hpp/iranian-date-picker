eae18f0b05ef39cca220f945bc4f9a53577f87c3
Arman <arman.hassanpoor2000@gmail.com>
2024-10-02 07:34:04 +0330

    remove newlines

--- src/types.ts
+++ src/types.ts
@@ -2,15 +2,10 @@ import '@emotion/react'
 import type { Radius } from './style/radius'
 
 export type DatePickerValue = number | Date | string
-
 export type Locales = 'fa' | 'en'
-
 export type Directions = 'rtl' | 'ltr'
-
 export type DaysRange = 0 | 1 | 2 | 3 | 4 | 5 | 6
-
 export type Pickers = 'days' | 'year' | 'month'
-
 export type Positions = 'right' | 'left' | 'center'
 
 export interface BaseProps {
